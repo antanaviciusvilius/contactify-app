f23011087a2d82e8b99e5fbe76d8adf12fa51046
ViliusAn <vilius.antanavicius123@gmail.com>
2023-11-19 21:41:47 +0200

    Deploy project to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import react from '@vitejs/plugin-react'
-import sass from 'sass'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import sass from 'sass';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        implementation: sass,
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          implementation: sass,
+        },
       },
     },
-  },
-})
+    base: '/',
+  };
+
+  if (command !== 'serve') {
+    config.base = '/contactify-app/';
+  }
+
+  return config;
+});
